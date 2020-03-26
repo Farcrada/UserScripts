d6afb1a15097cac2a64c0ffaeadc965392a46e60
Farcrada Withman <Farcrada@gmail.com>
2020-03-26 03:37:32 +0100

    Update open-spotify-app.user.js

--- Open Spotify app/open-spotify-app.user.js
+++ Open Spotify app/open-spotify-app.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/Farcrada/UserScripts
 // @homepage     https://github.com/Farcrada/UserScripts/tree/master/Open%20Spotify%20app
 // @updateURL    https://github.com/Farcrada/UserScripts/raw/master/Open%20Spotify%20app/open-spotify-app.user.js
-// @description  This userscript redirects open.spotify.com links to the desktop app
+// @description  This userscript redirects open.spotify.com links to the desktop app instead of the web player.
 // @author       Farcrada
 // @include      http://open.spotify.com/*
 // @include      https://open.spotify.com/*
